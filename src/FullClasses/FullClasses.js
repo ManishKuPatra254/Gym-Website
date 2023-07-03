823c4020e8186f09aff6f6f735bb8fb2bbc639dd
ManishKuPatra254 <manishpatra254@gmail.com>
2023-07-03 13:11:10 +0530

    added nav classes and done the cards

--- src/FullClasses/FullClasses.js
+++ src/FullClasses/FullClasses.js
@@ -1,10 +1,35 @@
 import style from './FullClasses.module.css';
-
-import React, { Fragment } from 'react'
+import { fullClassesData } from './FullClassesData';
+import React, { Fragment } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 export function FullClasses() {
+
+    function handleClickTeamPop() {
+        toast('Joined')
+    }
+
     return (
         <Fragment>
+            <div className={style.heading5}>
+                <h4>Classes We Schedule</h4>
+            </div>
+
+            <div className={style.met_main_team}>
+                {
+                    fullClassesData.map((itemsCard) => (
+                        <div key={itemsCard.id} className={style.sub_name}>
+                            <img src={itemsCard.images} alt="" />
+                            <p>{itemsCard.names}</p>
+                            <span>{itemsCard.prof}</span>
+                            <div className={style.btn_add}>
+                                <button variant="contained" onClick={handleClickTeamPop}>Join now</button>
+                                <Toaster />
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
 
         </Fragment>
     )
---
+++ src/FullClasses/FullClassesData.js
@@ -0,0 +1,63 @@
+export const fullClassesData = [
+    {
+        "id": 1,
+        "images": "https://images.pexels.com/photos/8436769/pexels-photo-8436769.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "names": "Yoga and Pilates Classes:",
+        "prof": " These classes emphasize flexibility, core strength, balance, and mindfulness.",
+
+    },
+    {
+        "id": 2,
+        "images": "https://images.pexels.com/photos/8436400/pexels-photo-8436400.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "names": "Group Fitness Classes",
+        "prof": "These classes focus on various fitness aspects.",
+    },
+    {
+        "id": 3,
+        "images": "https://images.pexels.com/photos/8436580/pexels-photo-8436580.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "names": "Bootcamp Classes",
+        "prof": " Bootcamp-style workouts involve a combination of cardio & strength training",
+    },
+    {
+        "id": 4,
+        "images": "https://images.pexels.com/photos/5209197/pexels-photo-5209197.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "names": "Power lifting",
+        "prof": "The goal of powerlifting is to lift the heaviest possible weight",
+    },
+    {
+        "id": 5,
+        "images": "https://images.pexels.com/photos/10518848/pexels-photo-10518848.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "names": "Muscle Stretching",
+        "prof": "Dynamic stretching involves active movements",
+    },
+    {
+        "id": 6,
+        "images": "https://images.pexels.com/photos/8436407/pexels-photo-8436407.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "names": "Calisthenics Classes",
+        "prof": "It is a form of exercise that utilizes to build strength, flexibility,",
+
+    },
+    {
+        "id": 7,
+        "images": "https://www.linkpicture.com/q/Cropped_Image_4.png",
+        "names": "Pregnancy Warm Classes ",
+        "prof": "Exercising during pregnancy can be benefit both mother and the baby,",
+    },
+    {
+        "id": 8,
+        "images": "https://images.pexels.com/photos/2345293/pexels-photo-2345293.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "names": "Zumba Classes",
+        "prof": " It is a popular group fitness activity that combines energetic dance movements",
+    },
+    {
+        "id": 9,
+        "images": "https://img.freepik.com/free-photo/two-little-girl-sitting-back-back-exercising-ball-together_23-2148185950.jpg?w=740&t=st=1688369862~exp=1688370462~hmac=c0cecc7b6494130206ff3f946fdfbc07a1a9a69dd15993207f87054e42323659",
+        "names": "Kids Fun Classes",
+        "prof": "Take kids to our gym for activities like swinging, climbing, and playing in gym.",
+    },
+]
+
+
+
+
+
