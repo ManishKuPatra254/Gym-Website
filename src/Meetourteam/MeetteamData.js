1f1717efb2746168c0546252fe4bcbc86f05764e
ManishKuPatra254 <manishpatra254@gmail.com>
2023-07-04 11:55:37 +0530

    added all the images in teams

--- src/Meetourteam/MeetteamData.js
+++ src/Meetourteam/MeetteamData.js
@@ -1,32 +1,32 @@
 export const meetOurTeamData = [
     {
         "id": 1,
-        "images": "https://cdn.inflact.com/media/352893783_804965537513858_2357062383799314358_n.jpg?url=https%3A%2F%2Fscontent.cdninstagram.com%2Fv%2Ft51.2885-15%2F352893783_804965537513858_2357062383799314358_n.jpg%3Fstp%3Ddst-jpg_e35_p1080x1080%26_nc_ht%3Dscontent.cdninstagram.com%26_nc_cat%3D102%26_nc_ohc%3DtFd_WqxK1JgAX87K1LB%26edm%3DAPs17CUBAAAA%26ccb%3D7-5%26ig_cache_key%3DMzEyNDA3NDk3Njk1NTc3NzMzMg%253D%253D.2-ccb7-5%26oh%3D00_AfBn360_CEVQO51inHM2bRtbOH_aChYHRGBv99p7fiKW0A%26oe%3D64A443B6%26_nc_sid%3D10d13b&time=1688144400&key=927eeda662cb1387cc81a1f2f9f324a5",
+        "images": "https://images.pexels.com/photos/896059/pexels-photo-896059.jpeg?auto=compress&cs=tinysrgb&w=600",
         "names": "Saket Gokhale",
         "prof": "Strength and Conditioning Coach",
 
     },
     {
         "id": 2,
-        "images": "https://cdn.inflact.com/media/351053159_1626712027812794_8654342984831154756_n.jpg?url=https%3A%2F%2Fscontent.cdninstagram.com%2Fv%2Ft51.2885-15%2F351053159_1626712027812794_8654342984831154756_n.jpg%3Fstp%3Ddst-jpg_e35_p1080x1080%26_nc_ht%3Dscontent.cdninstagram.com%26_nc_cat%3D102%26_nc_ohc%3DprpsG8GtUOsAX99RMh3%26edm%3DAPs17CUBAAAA%26ccb%3D7-5%26oh%3D00_AfCmuoV7VJ1FsZAt1SruRXcnB7oRiWBnnyNcXG2S5onEdQ%26oe%3D64A39CF9%26_nc_sid%3D10d13b&time=1688144400&key=e03153007e213f0c1afde95d9f9482b9",
+        "images": "https://images.pexels.com/photos/2204201/pexels-photo-2204201.jpeg?auto=compress&cs=tinysrgb&w=600",
         "names": "Sourav Singha Rajput",
         "prof": "Strength and Conditioning Coach",
     },
     {
         "id": 3,
-        "images": "https://cdn.inflact.com/media/341781404_533545742191486_648252874464700137_n.jpg?url=https%3A%2F%2Fscontent.cdninstagram.com%2Fv%2Ft51.2885-15%2F341781404_533545742191486_648252874464700137_n.jpg%3Fstp%3Ddst-jpg_e35_p1080x1080%26_nc_ht%3Dscontent.cdninstagram.com%26_nc_cat%3D110%26_nc_ohc%3DQhfeYl54z68AX8PkGNu%26edm%3DAPs17CUBAAAA%26ccb%3D7-5%26oh%3D00_AfCwCj5ptVRtQJkWJHrfJMMk8gvdZkNw07IHWLN-8K7uVg%26oe%3D64A428AF%26_nc_sid%3D10d13b&time=1688144400&key=ca6c857a10ccbf1c2f9b0c0a94cf2b1b",
+        "images": "https://images.pexels.com/photos/16666831/pexels-photo-16666831/free-photo-of-muscular-man-posing-in-gym.jpeg?auto=compress&cs=tinysrgb&w=600",
         "names": "Guru Mann",
         "prof" : " Proper Nutritionist & Dietitian",
     },
     {
         "id": 4,
-        "images": "https://cdn.inflact.com/media/356627266_268602115760230_2316481370062620045_n.jpg?url=https%3A%2F%2Fscontent.cdninstagram.com%2Fv%2Ft51.2885-15%2F356627266_268602115760230_2316481370062620045_n.jpg%3Fstp%3Ddst-jpg_e35_p1080x1080%26_nc_ht%3Dinstagram.fhyd14-2.fna.fbcdn.net%26_nc_cat%3D1%26_nc_ohc%3DNdlp69hHwzQAX-DBiDf%26edm%3DAP_V10EBAAAA%26ccb%3D7-5%26ig_cache_key%3DMzEzNTE2NjIxOTQ1MjYwNTU2Ng%253D%253D.2-ccb7-5%26oh%3D00_AfCE6XKfvoHQpods2mGPKusXSsUfre6JQ059SfyArPJ-8w%26oe%3D64A308D0%26_nc_sid%3D2999b8&time=1688144400&key=d0d7d7c39f45520db2940c6c012fb384",
+        "images": "https://images.pexels.com/photos/6389075/pexels-photo-6389075.jpeg?auto=compress&cs=tinysrgb&w=600",
         "names": "Abhishek Yadav",
         "prof": "Olympia amateur 2018🏅2021🥉",
     },
     {
         "id": 5,
-        "images": "https://cdn.inflact.com/media/350242390_953558829401428_6945726001752862306_n.jpg?url=https%3A%2F%2Fscontent.cdninstagram.com%2Fv%2Ft51.2885-15%2F350242390_953558829401428_6945726001752862306_n.jpg%3Fstp%3Ddst-jpg_e35_p1080x1080%26_nc_ht%3Dscontent.cdninstagram.com%26_nc_cat%3D104%26_nc_ohc%3DCugsgXvHiLMAX_zduND%26edm%3DAPs17CUBAAAA%26ccb%3D7-5%26ig_cache_key%3DMzExMzQzNzU2NjkzOTU2MTg0NQ%253D%253D.2-ccb7-5%26oh%3D00_AfArT6cXYms1Zt4LJlbNou7ML_ovKJq_sqdvM6OI8tX31g%26oe%3D64A315D7%26_nc_sid%3D10d13b&time=1688144400&key=68b180abb86b83251bf7854b0fdb287c",
+        "images": "https://images.pexels.com/photos/6550857/pexels-photo-6550857.jpeg?auto=compress&cs=tinysrgb&w=600",
         "names": "Jeet Selal",
         "prof": "CrossFit Trainer",
     },
